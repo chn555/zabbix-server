a43d452957d4d94601aff7804d206e3e7332d457
chn555 <chn566@gmail.com>
2018-08-04 18:21:11 +0300

    fourth

--- zabbix-server.sh
+++ zabbix-server.sh
@@ -22,9 +22,12 @@ systemctl enable mariadb --now
  y
  EOF
 
+echo "Enter root SQL password"
 mysql -u root -p -e "create database zabbix character set utf8 collate utf8_bin;"
 mysql -u root -p -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';"
 
+
+echo "Enter zabbix SQL password"
 zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -p zabbix
 
 sed -i -e 's/# DBHost=localhost/DBHost=localhost/g' /etc/zabbix/zabbix_server.conf
